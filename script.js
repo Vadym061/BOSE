8c555fcbee8d88fade91317d9c4f9d79e68a98a5
Vadym Shtyra <ximik061@gmail.com>
2023-12-18 23:26:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const headerBurgerMenuIcon = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".menu");
+const close = document.querySelector('.burger__close');
+
+
+function clickBurgerMenu() {
+    headerBurgerMenuIcon.addEventListener("click", () => {
+        document.querySelector(".menu").classList.add("active")
+    })
+
+
+    close.addEventListener("click", () => {
+        burgerMenu.classList.remove("active");
+            headerBurgerMenuIcon.classList.remove("active");
+    });
+};
+
+clickBurgerMenu();
